ed887e75c4b96e9484aa67cb3c3a9c74316329b9
gmrms <guilhermemorum@gmail.com>
2023-03-06 15:02:47 -0300

    NIP-15 (#5)
    Co-authored-by: Guilherme Morum Santos <g@numih.com>

---
+++ payload/src/main/kotlin/com/nipsr/payload/nips/NIP_15.kt
@@ -0,0 +1,10 @@
+package com.nipsr.payload.nips
+
+/**
+ * NIP-15: End of Stored Events Notice
+ *
+ * Read more: https://github.com/nostr-protocol/nips/blob/master/15.md
+ */
+@Target(AnnotationTarget.CLASS, AnnotationTarget.EXPRESSION, AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.SOURCE)
+annotation class NIP_15
--- relay/src/main/kotlin/com/nipsr/relay/message/MessageType.kt
+++ relay/src/main/kotlin/com/nipsr/relay/message/MessageType.kt
@@ -1,11 +1,14 @@
 package com.nipsr.relay.message
 
 import com.nipsr.payload.nips.NIP_01
+import com.nipsr.payload.nips.NIP_15
 
 @NIP_01
-enum class MessageType(val client: Boolean, val server: Boolean) {
-    EVENT(true, true),
-    REQ(true, false),
-    CLOSE(true, false),
-    NOTICE(false, true)
+enum class MessageType {
+    EVENT,
+    REQ,
+    CLOSE,
+    NOTICE,
+    @NIP_15
+    EOSE
 }
