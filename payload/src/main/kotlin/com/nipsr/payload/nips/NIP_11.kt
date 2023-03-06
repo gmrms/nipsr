89fc0e9f689c5aec7f2102565c58097799164da1
gmrms <guilhermemorum@gmail.com>
2023-03-06 11:02:05 -0300

    NIP-11 (#3)
    Co-authored-by: Guilherme Morum Santos <g@numih.com>

---
+++ payload/src/main/kotlin/com/nipsr/payload/nips/NIP_11.kt
@@ -0,0 +1,8 @@
+package com.nipsr.payload.nips
+
+/**
+ * NIP-11: Relay Information Document
+ *
+ * Read more: https://github.com/nostr-protocol/nips/blob/master/11.md
+ */
+annotation class NIP_11
---
+++ relay/src/main/kotlin/com/nipsr/relay/model/RelayMetadata.kt
@@ -0,0 +1,14 @@
+package com.nipsr.relay.model
+
+import com.nipsr.payload.nips.NIP_11
+
+@NIP_11
+data class RelayMetadata(
+    val name: String,
+    val description: String,
+    val pubkey: String,
+    val contact: String,
+    val supported_nips: List<String>,
+    val software: String,
+    val version: String
+)
